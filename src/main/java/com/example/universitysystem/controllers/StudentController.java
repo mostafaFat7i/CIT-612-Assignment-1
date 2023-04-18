9bab66caa62bf2566f46e45e475f36ab12054c12
Mohamed Ibrahim Ragab <113059624+MohamedIbrahim1708@users.noreply.github.com>
2023-04-18 16:46:41 +0200

    Update StudentController.java

--- src/main/java/com/example/universitysystem/controllers/StudentController.java
+++ src/main/java/com/example/universitysystem/controllers/StudentController.java
@@ -1,7 +1,41 @@
 package com.example.universitysystem.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.universitysystem.entities.Student;
+import com.example.universitysystem.services.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class StudentController {
+    @Autowired
+    private StudentService studentService;
+
+    // Insert New Student
+    @PostMapping("/student")
+    public String saveStudent(@RequestBody Student student){
+        studentService.saveStudent(student);
+        return "Student Added successfully";
+    }
+    // Get all Student
+    @GetMapping("/student")
+    public List<Student> fetchStudents(){
+        return studentService.fetchStudents();
+    }
+    // Get Student By ID
+    @GetMapping("/{id}")
+    public Student getStudentById(@PathVariable Long id) {
+        return studentService.getStudentById(id);
+    }
+    // Update Student
+    @PutMapping("/{id}")
+    public Student updateStudent(@PathVariable Long id, @RequestBody Student student) {
+        return studentService.updateStudent(id, student);
+    }
+    // Delete Student
+    @DeleteMapping("/{id}")
+    public void deleteStudent(@PathVariable Long id) {
+        studentService.deleteStudent(id);
+    }
 }
